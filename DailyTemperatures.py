567f00eceb98312c201f1d54d4e85a0c2b97e12d
Yienisha Abeyratne <97560226+yienishaa@users.noreply.github.com>
2024-05-03 17:15:46 -0400

    739 Create DailyTemperatures.py

---
+++ DailyTemperatures.py
@@ -0,0 +1,19 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        #monotonic decreasing problem or equal
+        stack = [temperatures[0]]
+        index_stack = [0]
+        answer = [0]*len(temperatures)
+
+        for i in range(1,len(temperatures)):
+            if temperatures[i] > stack[-1]:
+                for j in range(len(stack)-1,-1,-1):
+                    if stack[j] < temperatures[i]:
+                        stack.pop()
+                        x = index_stack.pop()
+                        answer[x] = i-x
+            
+            stack.append(temperatures[i])
+            index_stack.append(i)
+
+        return answer
